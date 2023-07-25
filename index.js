38f82c934a0cd92787edffee78ae4a1001575844
SachinBalagam <balagamsachin337@gmail.com>
2023-07-25 04:22:09 +0000

    Final Commit

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const Box = props => {
-  //  Write your code here.
-}
+const Box = (props) => {
+  const { title, styling } = props;
+  return (
+    <div className={`box ${styling}`}>
+      <h1 className="title">{title}</h1>
+    </div>
+  );
+};
 
 const element = (
-  //  Write your code here. 
-)
+  <div className="bg-container">
+    <h1 className="heading">Boxes</h1>
+    <div className="cards-container">
+      <Box title="Small" styling="small" />
+      <Box title="Medium" styling="medium" />
+      <Box title="Large" styling="large" />
+    </div>
+  </div>
+);
 
 ReactDOM.render(element, document.getElementById("root"));
